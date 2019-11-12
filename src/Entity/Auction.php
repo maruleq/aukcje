a03684ea4d302f287021026400e3bd0e89dd5d63
Marek Grabowski <marek@grabowscy.eu>
2019-11-12 22:56:25 +0100

    add new fields to entity

--- src/Entity/Auction.php
+++ src/Entity/Auction.php
@@ -5,69 +5,285 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
+ *  Auction
+ *
+ * @ORM\Table(name="auction")
  * @ORM\Entity(repositoryClass="App\Repository\AuctionRepository")
  */
 class Auction
 {
+    const STATUS_ACTIVE = "active";
+    const STATUS_FINISHED = "finished";
+    const STATUS_CANCELLED = "cancelled";
+    
     /**
+     * @var int
+     * 
      * @ORM\Id()
      * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
+     * @ORM\Column(name="id", type="integer")
      */
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @var string
+     * 
+     * @ORM\Column(name="title", type="string", length=255)
      */
     private $title;
 
     /**
-     * @ORM\Column(type="text")
+     * @var string
+     * 
+     * @ORM\Column(name="description", type="text")
      */
     private $description;
 
     /**
-     * @ORM\Column(type="decimal", precision=10, scale=2)
+     * @var float
+     * 
+     * @ORM\Column(name="price", type="decimal", precision=10, scale=2)
      */
     private $price;
+    
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="starting_price", type="decimal", precision=10, scale=2)
+     */
+    private $startingPrice;
+    
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+    
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime")
+     */
+    private $updatedAt;
+    
+     /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="expires_at", type="datetime")
+     */
+    private $expiresAt;
+    
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="status", type="string", length=10)
+     */
+    private $status;
 
-    public function getId(): ?int
+    
+    
+    /* Getters and Setters */
+    
+    
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
     {
         return $this->id;
     }
 
-    public function getTitle(): ?string
+    /**
+     * Get title.
+     *
+     * @return string
+     */
+    public function getTitle()
     {
         return $this->title;
     }
 
-    public function setTitle(string $title): self
+    /**
+     * Set title.
+     *
+     * @param string $title
+     *
+     * @return Auction
+     */
+    public function setTitle(string $title)
     {
         $this->title = $title;
 
         return $this;
     }
 
-    public function getDescription(): ?string
+    /**
+     * Get description.
+     *
+     * @return string
+     */
+    public function getDescription()
     {
         return $this->description;
     }
 
-    public function setDescription(string $description): self
+    /**
+     * Set description.
+     *
+     * @param string $description
+     *
+     * @return Auction
+     */
+    public function setDescription(string $description)
     {
         $this->description = $description;
 
         return $this;
     }
 
-    public function getPrice(): ?string
+    /**
+     * Get price.
+     *
+     * @return string
+     */
+    public function getPrice()
     {
         return $this->price;
     }
 
-    public function setPrice(string $price): self
+    /**
+     * Set price.
+     *
+     * @param string $price
+     *
+     * @return Auction
+     */
+    public function setPrice(string $price)
     {
         $this->price = $price;
+
+        return $this;
+    }
+    
+    /**
+     * Get startingPrice.
+     *
+     * @return float
+     */
+    public function getStartingPrice()
+    {
+        return $this->startingPrice;
+    }
+    
+    /**
+     * Set startingPrice.
+     *
+     * @param float $startingPrice
+     *
+     * @return Auction
+     */
+    public function setStartingPrice($startingPrice)
+    {
+        $this->startingPrice = $startingPrice;
+
+        return $this;
+    }
+    
+     /**
+     * Get createdAt.
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+    
+    /**
+     * Set createdAt.
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return $this
+     */
+    public function setCreatedAt(\DateTime $createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+    
+    /**
+     * Get updatedAt.
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+    
+    /**
+     * Set updatedAt.
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return $this
+     */
+    public function setUpdatedAt(\DateTime $updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+    
+    /**
+     * Get expiresAt.
+     *
+     * @return \DateTime
+     */
+    public function getExpiresAt()
+    {
+        return $this->expiresAt;
+    }
+    
+    /**
+     * Set expiresAt.
+     *
+     * @param \DateTime $expiresAt
+     *
+     * @return $this
+     */
+    public function setExpiresAt(\DateTime $expiresAt)
+    {
+        $this->expiresAt = $expiresAt;
+
+        return $this;
+    }
+    
+    /**
+     * Get status.
+     *
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+    
+    /**
+     * Set status.
+     *
+     * @param string $status
+     *
+     * @return $this
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
 
         return $this;
     }
